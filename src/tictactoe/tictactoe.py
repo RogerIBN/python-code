b9301ad1b85076e1f955462b5bbb0d6e95214d19
Roger Barrera <rogaba_2@hotmail.com>
2022-06-28 08:19:52 -0500

    update tictactoe

--- src/tictactoe/tictactoe.py
+++ src/tictactoe/tictactoe.py
@@ -2,8 +2,12 @@
 
 """Tic tac toe game"""
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
 import numpy as np
+
+BOARD_SIZE = 3
+Coordinate = tuple[int]
 
 
 @dataclass
@@ -12,14 +16,24 @@ class Player:
 
     name: str
     symbol: str
-    last_move: tuple[int] = None, None
+    last_move: Coordinate = None, None
+    valid_positions: set[tuple[int]] = field(
+        default_factory=lambda: set(np.ndindex((BOARD_SIZE, BOARD_SIZE)))
+    )
 
-    def get_next_coordinate(self) -> tuple[int]:
+    def get_next_coordinate(self) -> Coordinate:
         """Get next coordinate for the player
         Returns:
             tuple(int): The next coordinate (y, x)
         """
-        current_move = int(input("Posición y: ")), int(input("Posición x: "))
+        current_move = tuple(
+            int(axis) for axis in input("Posición -> y, x: ").split(",")
+        )
+
+        if current_move not in self.valid_positions:
+            raise ValueError(
+                f"{current_move} -> y or x coordinate is not in [0, {BOARD_SIZE + 1}) range"
+            )
         self.last_move = current_move
         return self.last_move
 
@@ -27,23 +41,28 @@ class Player:
 class TicTacToe:
     """Tic tac toe game class"""
 
-    valid_positions = {(pos_y, pos_x) for pos_y in range(3) for pos_x in range(3)}
-
-    def __init__(self, player_1: Player, player_2: Player) -> None:
+    def __init__(self, current_turn: Player, next_turn: Player) -> None:
         """Initialize board"""
-        self.board = np.zeros((3, 3), dtype=str)
+        self.board = np.zeros((BOARD_SIZE, BOARD_SIZE), dtype=str)
         self.board[:] = " "
-        self.current_turn = player_1
-        self.next_turn = player_2
+        self.current_turn = current_turn
+        self.next_turn = next_turn
+
+    def __str__(self) -> str:
+        """Return board"""
+        board = str()
+        for index, row in enumerate(self.board):
+            if index < (BOARD_SIZE - 1):
+                board += f"{'  |  '.join(row)}\n"
+                board += f"-- + {'--- + ' * (BOARD_SIZE - 2)}--\n"
+            else:
+                board += "  |  ".join(row)
+        return board
 
     def _print_turn(self) -> None:
         """Print turn"""
         print(f"{self.current_turn.name}'s turn ({self.current_turn.symbol})")
         print(self)
-
-    def __str__(self) -> str:
-        """Return board"""
-        return f"{self.board[0]}\n{self.board[1]}\n{self.board[2]}"
 
     def _check_win(self) -> bool:
         """Check if there is a winner"""
@@ -65,15 +84,15 @@ class TicTacToe:
         """Check if there is a tie"""
         return (self.board != " ").all()
 
-    def _check_valid(self, coordinate: tuple[int]) -> bool:
+    def _check_valid(self, coordinate: Coordinate) -> bool:
         """Check if position is valid"""
-        return coordinate in self.valid_positions and self.board[coordinate] == " "
+        return self.board[coordinate] == " "
 
-    def _update_board(self, coordinate: tuple[int]) -> bool:
+    def _update_board(self, coordinate: Coordinate) -> bool:
         """Return if game ended and updates board
 
         Args:
-            coordinate (tuple[int]): (y, x) coordinate
+            coordinate (Coordinate): (y, x) coordinate
 
         Returns:
             bool: True if game ended
@@ -98,7 +117,11 @@ class TicTacToe:
         game_ended = False
         while not game_ended:
             self._print_turn()
-            coordinate = self.current_turn.get_next_coordinate()
+            try:
+                coordinate = self.current_turn.get_next_coordinate()
+            except ValueError as error:
+                print(error)
+                continue
             game_ended = self._update_board(coordinate)
         print(self)
 
@@ -106,8 +129,8 @@ class TicTacToe:
 # %%
 def main():
     """Main function"""
-    player_1 = Player("Roger", "X")
-    player_2 = Player("Karla", "O")
+    player_1 = Player("Roger", "\N{Ballot X}")
+    player_2 = Player("Karla", "\N{White Circle}")
     game = TicTacToe(player_1, player_2)
     game.start()
 
