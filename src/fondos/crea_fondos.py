b2244f175f125fd62426a6a6d1489453acbd6865
Roger Barrera <rogaba_2@hotmail.com>
2023-02-15 12:27:47 -0600

    refactor: :recycle: translate to english

--- src/fondos/crea_fondos.py
+++ src/fondos/crea_fondos.py
@@ -5,204 +5,231 @@ from typing import Iterable
 import cv2 as cv
 import numpy as np
 
-Pixel = Iterable[int, int, int]
-Imagen = np.ndarray
+Pixel = Iterable[int]
+Image = np.ndarray
 
 
-def dibuja_fondo(color: Pixel, tamano: Iterable[int, int] = (1920, 1080)) -> Imagen:
-    """Crea una imagen RGB de un solo un color plano.
-
-    Args:
-        color (pixel): Color descrito en RGB.
-        tamaño (list[int, int]): Tamaño de la imagen.
-
-    Returns:
-        imagen: Imagen RGB de un color.
+def drawn_background(color: Pixel, size: Iterable[int] = (1920, 1080)) -> Image:
     """
-    ancho, alto = tamano
-    return np.full((alto, ancho, 3), color, dtype="uint8")
+    Crea una imagen RGB de un solo un color plano.
 
+    Parameters
+    ----------
+    color : Pixel
+        Color descrito en RGB
+    size : Iterable[int], optional
+        Tamaño de la imagen, by default (1920, 1080)
 
-def dibuja_rejilla(
-    img: Imagen, color: Pixel, espaciado: int = 30, grosor: int = 1
-) -> Imagen:
-    """Dibuja una rejilla de lineas continuas en una imagen RGB.
-
-    Args:
-        img (imagen): Imagen a dibujar.
-        color (pixel): Color descrito en RGB
-        espaciado (int, optional): Espacio entre lineas en
-        pixels. Defaults to 30.
-        grosor (int, optional): Grueso de las lineas. Defaults to 1.
-
-    Returns:
-        imagen: imagen con la rejilla de lineas
+    Returns
+    -------
+    Image
+        Imagen RGB de un color
     """
-    alto, ancho, _ = img.shape
-    for fila, columna in product(range(0, alto, espaciado), range(0, ancho, espaciado)):
+    width, height = size
+    return np.full((height, width, 3), color, dtype="uint8")
+
+
+def drawn_grid(img: Image, color: Pixel, gap: int = 30, thickness: int = 1) -> Image:
+    """
+    Dibuja una rejilla de lineas continuas en una imagen RGB
+
+    Parameters
+    ----------
+    img : Image
+        Imagen a dibujar
+    color : Pixel
+        Color descrito en RGB
+    gap : int, optional
+        Espacio entre lineas, by default 30
+    thickness : int, optional
+        Grueso de las lineas, by default 1
+
+    Returns
+    -------
+    Image
+        Imagen con la rejilla de lineas
+    """
+    height, width, _ = img.shape
+    for row, column in product(range(0, height, gap), range(0, width, gap)):
         # Lineas horizontales
-        cv.line(img, (0, fila), (ancho, fila), color=color, thickness=grosor)
+        cv.line(img, (0, row), (width, row), color=color, thickness=thickness)
         # Lineas verticales
-        cv.line(img, (columna, 0), (columna, alto), color=color, thickness=grosor)
+        cv.line(img, (column, 0), (column, height), color=color, thickness=thickness)
 
     # Última linea horizontal inferior
-    cv.line(img, (0, alto - 1), (ancho - 1, alto - 1), color=color, thickness=grosor)
+    cv.line(
+        img, (0, height - 1), (width - 1, height - 1), color=color, thickness=thickness
+    )
     # Última linea vertical derecha
-    cv.line(img, (ancho - 1, 0), (ancho - 1, alto - 1), color=color, thickness=grosor)
+    cv.line(
+        img, (width - 1, 0), (width - 1, height - 1), color=color, thickness=thickness
+    )
     return img
 
 
-def dibuja_puntos(
-    img: Imagen, color: Pixel, espaciado: int = 30, radio: int = 1
-) -> Imagen:
-    """Dibuja una rejilla de puntos en una imagen
-
-    Args:
-        img (imagen): Imagen a dibujar.
-        color (pixel): Color en RGB
-        espaciado (int, optional): Espacio entre puntos en pixels.
-        Defaults to 30.
-        radio (int, optional): Radio de los puntos en pixels. Defaults to 1.
-
-    Returns:
-        imagen: Imagen con la rejilla de puntos
+def drawn_dots(img: Image, color: Pixel, gap: int = 30, radius: int = 1) -> Image:
     """
-    alto, ancho, _ = img.shape
-    for fila, columna in product(
-        range(espaciado, alto, espaciado), range(espaciado, ancho, espaciado)
-    ):
-        cv.circle(img, center=(columna, fila), radius=radio, color=color, thickness=-1)
+    Dibuja una rejilla de puntos en una imagen
+
+    Parameters
+    ----------
+    img : Image
+        Imagen a dibujar
+    color : Pixel
+        Color en RGB
+    gap : int, optional
+        Espacio entre puntos en pixeles, by default 30
+    radius : int, optional
+        Radio de los puntos en pixeles, by default 1
+
+    Returns
+    -------
+    Image
+        Imagen con la rejilla de puntos
+    """
+    height, width, _ = img.shape
+    for row, column in product(range(gap, height, gap), range(gap, width, gap)):
+        cv.circle(img, center=(column, row), radius=radius, color=color, thickness=-1)
     return img
 
 
-def cambiar_tamano(img: Imagen, cambio: float) -> Imagen:
-    """Cambia el tamaño de una imagen con un factor de proporción
-
-    Args:
-        imagen (imagen): Imagen a cambiar.
-        cambio (float): Factor de cambio de tamaño
-        0 < cambio < 1 = reducción
-        cambio > 1 = aumento
-
-    Returns:
-        imagen: Imagen con el tamaño cambiado.
+def change_size(img: Image, rate: float) -> Image:
     """
-    alto, ancho, _ = img.shape
+    Cambia el tamaño de una imagen con un factor de proporción
+
+    Parameters
+    ----------
+    img : Image
+        Imagen a cambiar
+    rate : float
+        Factor de cambio de tamaño (0 < rate < 1 - reducción, rate > 1 - aumento)
+
+    Returns
+    -------
+    Image
+        Imagen con el tamaño cambiado
+    """
+    height, width, _ = img.shape
     # Redondea los nuevos tamaños a un valor entero
-    ancho = int(ancho * cambio)
-    alto = int(alto * cambio)
-    return cv.resize(img, (ancho, alto), interpolation=cv.INTER_AREA)
+    width = int(width * rate)
+    height = int(height * rate)
+    return cv.resize(img, (width, height), interpolation=cv.INTER_AREA)
 
 
-def unir_imagenes(
-    primer_plano: Imagen, segundo_plano: Imagen, coordenadas: list[int, int]
-) -> Imagen:
-    """Unir dos imágenes en una sola.
+def join_images(
+    foreground: Image, background: Image, coordinates: Iterable[int]
+) -> Image:
+    """
+    Unir dos imágenes en una sola.
 
-    Args:
-        primer_plano (Imagen): La imagen que se va a unir.
-        segundo_plano (Imagen): El fondo que se va a unir.
-        coordenadas (list[int, int]): Posición de la imagen que se va a unir.
+    Parameters
+    ----------
+    foreground : Image
+        La imagen que se va a unir
+    background : Image
+        Segundo plano que se va a unir
+    coordinates : Iterable[int]
+        Posición de la imagen que se va a unir
 
-    Returns:
-        Imagen: _description_
+    Returns
+    -------
+    Image
+        Imagen con las figuras unidas
     """
     # Quiero colocar el logo en las coordenadas (x, y), así que
     # creo zoom_fondo_segundo_plano
-    x_coord, y_coord = coordenadas
-    alto, ancho, _ = primer_plano.shape
-    zoom_fondo_segundo_plano = segundo_plano[
-        y_coord : y_coord + alto, x_coord : x_coord + ancho
-    ]
+    x_coord, y_coord = coordinates
+    alto, ancho, _ = foreground.shape
+    background_zoomed = background[y_coord : y_coord + alto, x_coord : x_coord + ancho]
 
     # Ahora creo una máscara del primer plano y su máscara inversa también.
-    primer_plano_gris = cv.cvtColor(primer_plano, cv.COLOR_BGR2GRAY)
-    _, mascara = cv.threshold(primer_plano_gris, 10, 255, cv.THRESH_BINARY)
+    foreground_gray = cv.cvtColor(foreground, cv.COLOR_BGR2GRAY)
+    _, mask = cv.threshold(foreground_gray, 10, 255, cv.THRESH_BINARY)
     # mascara = cv.erode(mascara, None)
     # kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
     # mascara = cv.morphologyEx(mascara, cv.MORPH_OPEN, kernel)
-    mascara_inv = cv.bitwise_not(mascara)
+    mask_inv = cv.bitwise_not(mask)
 
     # Deja en negro (0, 0, 0) el area del logo en zoom_fondo_segundo_plano
-    recorte_zoom_fondo_segundo_plano = cv.bitwise_and(
-        zoom_fondo_segundo_plano, zoom_fondo_segundo_plano, mask=mascara_inv
+    background_cropped_zoomed = cv.bitwise_and(
+        background_zoomed, background_zoomed, mask=mask_inv
     )
 
     # Toma solo la región del logo en la imagen
-    recorte_frente_primer_plano = cv.bitwise_and(
-        primer_plano, primer_plano, mask=mascara
-    )
+    foreground_cropped = cv.bitwise_and(foreground, foreground, mask=mask)
 
     # Pon el logo en el zoom_fondo_segundo_plano y modifica la imagen del fondo
-    zoom_primer_y_segundo_plano = cv.add(
-        recorte_zoom_fondo_segundo_plano, recorte_frente_primer_plano
-    )
-    segundo_plano[
-        y_coord : y_coord + alto, x_coord : x_coord + ancho
-    ] = zoom_primer_y_segundo_plano
-    return segundo_plano
+    joined_zoomed = cv.add(background_cropped_zoomed, foreground_cropped)
+    background[y_coord : y_coord + alto, x_coord : x_coord + ancho] = joined_zoomed
+    return background
 
 
-def aplicar_vineta(img: Imagen, sigma: int = 200) -> Imagen:
-    """Aplica una viñeta a una imagen
+def apply_vignette(img: Image, sigma: int = 200) -> Image:
+    """
+    Aplicar una viñeta a una imagen
 
-    Args:
-        img (Imagen): La imagen a la que se le va a aplicar la viñeta.
-        sigma (int, optional): Valor de la intensidad. Defaults to 200.
+    Parameters
+    ----------
+    img : Image
+        La imagen a la que se le va a aplicar la viñeta
+    sigma : int, optional
+        Valor de la intensidad, by default 200
 
-    Returns:
-        Imagen: Imagen con la viñeta aplicada.
+    Returns
+    -------
+    Image
+        Imagen con la viñeta
     """
     # Calcular el alto y ancho de la imagen
-    alto, ancho, _ = img.shape
+    height, width, _ = img.shape
 
     # Genera una mascara de viñeta usando los
     # kernel gaussianos resultantes
-    x_kernel = cv.getGaussianKernel(ancho, sigma)
-    y_kernel = cv.getGaussianKernel(alto, sigma)
+    x_kernel = cv.getGaussianKernel(width, sigma)
+    y_kernel = cv.getGaussianKernel(height, sigma)
 
     # Generando la matriz del kernel resultante
     kernel = y_kernel * x_kernel.T
 
     # Creando una máscara y normalizándose usando una
     # función de numpy
-    mascara = 255 * kernel / np.linalg.norm(kernel)
+    mask = 255 * kernel / np.linalg.norm(kernel)
 
     # Aplicando la máscara a cada canal de la imagen
     for i in range(3):
-        img[:, :, i] *= mascara
+        img[:, :, i] *= mask
 
     return img
 
 
 def main() -> None:
     """Función principal"""
-    tamano = (1920, 1080)
-    color_fondo = [0] * 3
-    intensidad = 40
-    color_marca = [intensidad] * 3  # (27, 27, 27)
+    size = (1920, 1080)
+    background_color = [0] * 3
+    intensity = 40
+    grid_color = [intensity] * 3  # (27, 27, 27)
 
-    fondo = dibuja_fondo(color_fondo, tamano)
-    # fondo = dibuja_puntos(fondo, color_marca, 30, 2)
-    fondo = dibuja_rejilla(fondo, color_marca, 30, 2)
+    background = drawn_background(background_color, size)
+    # background = dibuja_puntos(background, grid_color, 30, 2)
+    background = drawn_grid(background, grid_color, 30, 2)
 
-    fondo = cv.cvtColor(fondo, cv.COLOR_RGB2BGR)
-    fondo = cv.GaussianBlur(fondo, (3, 3), 0)
-    # fondo = aplicar_viñeta(fondo)
+    background = cv.cvtColor(background, cv.COLOR_RGB2BGR)
+    background = cv.GaussianBlur(background, (3, 3), 0)
+    # background = aplicar_viñeta(background)
     # Invertir colores del fondo
-    # fondo = cv.bitwise_not(fondo)
+    # background = cv.bitwise_not(background)
 
     logo = cv.imread("src/fondos/images/alpha.png")
     # Cambiar tamaño de la imagen en primer plano
-    logo = cambiar_tamano(logo, 0.12)
+    logo = change_size(logo, 0.12)
     # Poner el logo en el fondo
-    fondo = unir_imagenes(logo, fondo, (1830, 990))
+    background = join_images(logo, background, (1830, 990))
 
     # Guardarla
-    # cv.imwrite('src/fondos/images/fondo_lineas_logo.png', fondo)
-    # cv.imwrite("src/fondos/images/fondo_blanco_puntos_logo.png", fondo)
-    # cv.imwrite("src/fondos/images/fondo_negro_puntos_logo.png", fondo)
-    cv.imwrite("src/fondos/images/fondo_negro_lineas_logo.png", fondo)
+    # cv.imwrite('src/fondos/images/fondo_lineas_logo.png', background)
+    # cv.imwrite("src/fondos/images/fondo_blanco_puntos_logo.png", background)
+    # cv.imwrite("src/fondos/images/fondo_negro_puntos_logo.png", background)
+    cv.imwrite("src/fondos/images/fondo_negro_lineas_logo.png", background)
 
 
 if __name__ == "__main__":
--- src/fondos/vineta.py
+++ src/fondos/vineta.py
@@ -1,50 +1,27 @@
-import numpy as np
-import cv2
+import cv2 as cv
 from crea_fondos import *
-
-
-def aplicar_vineta(imagen, sigma=200):
-    # Calcular el alto y ancho de la imagen
-    alto, ancho, _ = imagen.shape
-
-    # Genera una mascara de viñeta usando los
-    # kernel gaussianos resultantes
-    x_kernel = cv2.getGaussianKernel(ancho, sigma * ancho / alto)
-    y_kernel = cv2.getGaussianKernel(alto, sigma)
-
-    # Generando la matriz del kernel resultante
-    kernel = y_kernel * x_kernel.T
-
-    # Creando una máscara y normalizándose usando una
-    # función de numpy
-    máscara = 255 * kernel / np.linalg.norm(kernel)
-
-    # Aplicando la máscara a cada canal de la imagen
-    for i in np.arange(3):
-        imagen[:, :, i] = imagen[:, :, i] * máscara
-    return imagen
 
 
 def main():
     # Leer la imagen
-    imagen = cv2.imread("fondos/images/calle.jpg")
+    image = cv.imread("fondos/images/calle.jpg")
 
     # Cambiar el tamaño de la imagen
-    imagen = cambiar_tamano(imagen, 0.4)
+    image = change_size(image, 0.4)
 
-    cv2.imshow("Original", imagen)
+    cv.imshow("Original", image)
 
-    imagen = aplicar_vineta(imagen)
+    image = apply_vignette(image)
 
     # displaying the vignette filter image
-    cv2.imshow("VIGNETTE", imagen)
+    cv.imshow("VIGNETTE", image)
 
     # Maintain output window until
     # user presses a key
-    cv2.waitKey(0)
+    cv.waitKey(0)
 
     # Destroying present windows on screen
-    cv2.destroyAllWindows()
+    cv.destroyAllWindows()
 
 
 if __name__ == "__main__":
