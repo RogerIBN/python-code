eca5e06a7057c37e48af7ad628d31a45e02be331
Roger Barrera <rogaba_2@hotmail.com>
2023-02-15 12:33:26 -0600

    style: :truck: rename crea fondos file

--- src/fondos/vineta.py
+++ src/fondos/vineta.py
@@ -1,17 +1,23 @@
+"""
+Modulo para aplicar el filtro de vineta a una imagen
+"""
 import cv2 as cv
-from crea_fondos import *
+import background_maker as bg_maker
 
 
 def main():
+    """
+    Función principal
+    """
     # Leer la imagen
     image = cv.imread("fondos/images/calle.jpg")
 
     # Cambiar el tamaño de la imagen
-    image = change_size(image, 0.4)
+    image = bg_maker.change_size(image, 0.4)
 
     cv.imshow("Original", image)
 
-    image = apply_vignette(image)
+    image = bg_maker.apply_vignette(image)
 
     # displaying the vignette filter image
     cv.imshow("VIGNETTE", image)
