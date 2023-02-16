419a3c1a7a53b2887c42f56fde15e8ca7312e232
Roger Barrera <rogaba_2@hotmail.com>
2023-02-16 16:20:54 -0600

    refactor: :recycle: extract functions

--- src/subtitulos/subs_txt_to_srt.py
+++ src/subtitulos/subs_txt_to_srt.py
@@ -1,15 +1,11 @@
-# %%
 """Módulos para calcular el la duración del último subtítulo"""
-import sys
 from datetime import datetime, timedelta
 from pathlib import Path
-from typing import Optional, Sequence
-
-# %%
+from typing import Iterable, Optional
 
 
 def srt_from_raw_youtube(
-    input_file: str, name: Optional[str] = None, seg: float = 3
+    input_file: Path, output_file_name: Optional[str] = None, seg: float = 3
 ) -> None:
     """
     Crea un archivo de extensión srt tomando un archivo de script de youtube.
@@ -18,7 +14,7 @@ def srt_from_raw_youtube(
     ----------
     input_file : str
         Dirección del archivo.
-    name : Optional[str], optional
+    output_file_name : Optional[str], optional
         Nombre del archivo, by default None
     seg : float, optional
         Duración del último subtítulo, by default 3
@@ -27,81 +23,114 @@ def srt_from_raw_youtube(
     # pero con extensión .srt y en la misma dirección del
     # archivo original
 
-    output_file = Path(input_file).with_suffix(".srt")
-    output_file = output_file.with_stem(name) if name else output_file
+    output_file = get_output_file(input_file, output_file_name)
 
     # Lee los subtítulos y crea el nuevo archivo
-    with (
-        open(input_file, "r", encoding="utf-8") as txt,
-        open(output_file, "w", encoding="utf-8") as srt,
-    ):
-        # Separa los tiempos de las frases en una tupla para tiempos
-        # y en un generador para las frases
-        txt = tuple(line.rstrip() for line in txt.readlines())
-        # Impares
-        time_stamps = txt[::2]
-        # Pares
-        phrases = (phrase.capitalize() for phrase in txt[1::2])
+    with input_file.open("r", encoding="utf-8") as txt:
+        txt = [line.rstrip() for line in txt.readlines()]
 
-        for i, phrase in enumerate(phrases):
-            first_time = f"00:{time_stamps[i]},00"
-            second_time = get_second_time(time_stamps, i, seg)
-            # Escribe el número del subtítulo, la duración y la frase
-            srt.write(
-                f"""\
-{i + 1}
-{first_time} --> {second_time}
-{phrase}
+    # Separa los tiempos de las frases en una tupla para tiempos
+    # y en un generador para las frases
 
-"""
-            )
+    # Impares
+    begin_time_stamps = txt[::2]
+    # Pares
+    phrases = (phrase.capitalize() for phrase in txt[1::2])
+
+    captions = get_captions(begin_time_stamps, phrases, seg)
+
+    with output_file.open("w", encoding="utf-8") as srt:
+        srt.writelines(captions)
 
 
-def get_second_time(time_stamps: Sequence[str], i: int, last_second: float) -> str:
+def get_captions(
+    begin_time_stamps: list[str], phrases: Iterable[str], seg: float
+) -> list[str]:
     """
-    Obtiene el segundo tiempo de la lista de tiempos para el subtítulo actual.
+    Crear las líneas del archivo srt.
 
     Parameters
     ----------
-    time_stamps : Sequence[str]
-        Los tiempos de los subtítulos
-    i : int
-        Contador del subtítulo actual
-    last_second : float
-        Duración del último subtítulo
+    begin_time_stamps : list[str]
+        La lista de tiempos iniciales de los subtítulos.
+    phrases : Iterable[str]
+        El generador de frases.
+    seg : float
+        La duración del último subtítulo.
 
     Returns
     -------
-    str
-        Segundo tiempo
+    list[str]
+        La lista de líneas del archivo srt.
     """
-    try:
-        # Da formato a los tiempos
-        return f"00:{time_stamps[i + 1]},00"
+    end_time_stamps = get_end_time_stamps(begin_time_stamps, seg)
 
-    except IndexError:  # Justo cuando falte el último tiempo
-        # Tomando el último tiempo del archivo se le da formato
-        # y suma 'last_seg' segundos para obtener el momento cuando
-        # desaparece el último subtítulo
-        second_time = datetime.strptime(time_stamps[i], "%M:%S") + timedelta(
-            seconds=last_second
-        )
-        return f"{second_time:%H:%M:%S},00"
+    captions = []
+    for i, (begin_time_stamp, end_time_stamp, phrase) in enumerate(
+        zip(begin_time_stamps, end_time_stamps, phrases), start=1
+    ):
+        caption = f"""\
+{i}
+00:{begin_time_stamp},00 --> 00:{end_time_stamp},00
+{phrase}
+"""
+        captions.append(caption)
+    return captions
 
-    except ValueError:
-        # Si no se puede dar formato a los tiempos
-        print("Error: Tiempos inválidos")
-        sys.exit()
+
+def get_output_file(input_file: Path, output_file_name: Optional[str]) -> Path:
+    """
+    Genera la dirección del archivo de salida.
+
+    Parameters
+    ----------
+    input_file : Path
+        Dirección del archivo de entrada.
+    output_file_name : str
+        Nombre del archivo de salida.
+
+    Returns
+    -------
+    Path
+        Dirección del archivo de salida.
+    """
+    output_file = input_file.with_suffix(".srt")
+    output_file = (
+        output_file.with_stem(output_file_name) if output_file_name else output_file
+    )
+
+    return output_file
+
+
+def get_end_time_stamps(begin_time_stamps: list[str], seg: float = 3) -> list[str]:
+    """
+    Genera la lista de tiempos finales de los subtítulos.
+
+    Parameters
+    ----------
+    begin_time_stamps : list[str]
+        La lista de tiempos iniciales de los subtítulos.
+    seg : float, optional
+        El tiempo del último subtítulo, by default 3
+
+    Returns
+    -------
+    list[str]
+        La lista de tiempos finales de los subtítulos.
+    """
+    end_time_stamps = begin_time_stamps.copy()
+    end_time_stamps.pop(0)
+    last_time = datetime.strptime(begin_time_stamps[-1], "%M:%S") + timedelta(
+        seconds=seg
+    )
+    end_time_stamps.append(f"{last_time:%M:%S}")
+    return end_time_stamps
 
 
 def main() -> None:
     """Programa principal"""
-    srt_from_raw_youtube("src/subtitulos/yt_sub.txt")
+    srt_from_raw_youtube(Path("src/subtitulos/yt_sub.txt"))
 
-
-# %%
 
 if __name__ == "__main__":
     main()
-
-# %%
