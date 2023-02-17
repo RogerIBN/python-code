f7e709d0c5b4c03d161074c9c2fa051d107502dc
Roger Barrera <rogaba_2@hotmail.com>
2023-02-16 18:05:16 -0600

    refactor: :recycle: change to functional and add loggers

--- src/organizador_acta_curp/organizador_acta_curp.py
+++ src/organizador_acta_curp/organizador_acta_curp.py
@@ -1,104 +1,127 @@
-"""Librerías para administrar directorios y
-manipulación de imágenes
 """
-#%%
-
+Mueve las imágenes de actas de nacimiento y CURP de una carpeta a otra de acuerdo a su
+nombre y las reduce de tamaño.
+"""
+import logging
 from pathlib import Path
+from typing import Optional
 
 import cv2 as cv
 import numpy as np
 
 Mat = np.ndarray[int, np.dtype[np.generic]]
+# Crear logger---------------------------------------------------------------------
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
 
 
-class MueveCurpActa:
-    """Clase para mover archivos de una carpeta a otra"""
+def change_size(img: Mat, rate: float) -> Mat:
+    """
+    Cambia el tamaño de una imagen con un factor de proporción
 
-    def __init__(self, carpeta_origen: str):
-        """Constructor de la clase.
+    Parameters
+    ----------
+    img : Mat
+        Imagen a cambiar
+    rate : float
+        Factor de cambio de tamaño (0 < rate < 1 - reducción, rate > 1 - aumento)
 
-        Args:
-            carpeta_origen (str): Dirección de la carpeta de origen.
-        """
-        self.carpeta_origen = Path(carpeta_origen)
-        self.carpeta_destino = self.carpeta_origen.with_name(
-            f"{self.carpeta_origen.name} Carpetas"
-        )
+    Returns
+    -------
+    Mat
+        Imagen con el tamaño cambiado
+    """
+    height, width, _ = img.shape
+    # Redondea los nuevos tamaños a un valor entero
+    width = int(width * rate)
+    height = int(height * rate)
+    return cv.resize(img, (width, height), interpolation=cv.INTER_AREA)
 
-    @staticmethod
-    def cambiar_tamano(imagen: Mat, cambio: float) -> Mat:
-        """Cambia el tamaño de una imagen con
-        un factor de proporción
 
-        Args:
-            imagen (Mat): Imagen a cambiar.
-            cambio (float): Factor de cambio de tamaño
-            0 < cambio < 1 = reducción
-            cambio > 1 = aumento
+def make_output_document_path(document: Path, output_dir: Path) -> Path:
+    """
+    Crea las carpetas de destino para un archivo.
 
-        Returns:
-            Mat: Imagen con el tamaño cambiado.
-        """
-        alto, ancho, _ = imagen.shape
-        # Redondea los nuevos tamaños a un valor entero
-        ancho = int(ancho * cambio)
-        alto = int(alto * cambio)
-        return cv.resize(imagen, (ancho, alto), interpolation=cv.INTER_AREA)
+    Parameters
+    ----------
+    document : Path
+        Dirección del archivo.
+    output_dir : Path
+        Dirección de la carpeta de destino.
 
-    def _crear_destino(self, archivo: Path) -> Path:
-        """Crea las carpetas de destino para un archivo.
+    Returns
+    -------
+    Path
+        Dirección final del archivo con su nuevo nombre.
+    """
+    # Extrae el nombre del alumne
+    student_name = " ".join(document.name.split()[:-1])
 
-        Args:
-            archivo (Path): Dirección del archivo.
+    # Determina la ubicación de la carpeta del alumne
+    student_dir = output_dir / student_name
 
-        Returns:
-            Path: Dirección final del archivo con su nuevo nombre.
-        """
-        # Extrae el nombre del alumne
-        nombre_alumne = " ".join(archivo.name.split()[:-1])
+    # Crea la nueva carpeta sino existe
+    if not student_dir.exists():
+        logger.info("Procesando la carpeta de %s", student_name)
+    student_dir.mkdir(parents=True, exist_ok=True)
 
-        # Determina la ubicación de la carpeta del alumne
-        carpeta_alumne = self.carpeta_destino / nombre_alumne
+    # Crea la nueva dirección del archivo
+    return student_dir / document.name
 
-        # Crea la nueva carpeta sino existe
-        if not carpeta_alumne.exists():
-            print(f"Procesando la carpeta de {nombre_alumne}")
-        carpeta_alumne.mkdir(parents=True, exist_ok=True)
 
-        # Crea la nueva dirección del archivo
-        return carpeta_alumne / archivo.name
+def move_images(input_dir: Path, output_dir: Path, reduction: Optional[float]) -> None:
+    """
+    Mueve los archivos de la carpeta de origen a la carpeta de destino.
+    Los archivos pueden ser reducidos de tamaño o aumentados.
 
-    def mover_archivos(self, reduccion: float) -> None:
-        """Mueve los archivos de la carpeta de origen a la carpeta de destino.
+    Parameters
+    ----------
+    input_dir : Path
+        Carpeta de origen.
+    output_dir : Path
+        Carpeta de destino.
+    reduction : Optional[float]
+        Razón de reducción de tamaño. Si es None no se reduce el tamaño.
 
-        Args:
-            reduccion (float): Factor de cambio de tamaño
-            0 < cambio < 1 = reducción
-            cambio > 1 = aumento
-        """
+    See Also
+    --------
+    make_output_document_path : Crea las carpetas de destino para un archivo.
+    bg_maker.change_size : Cambia el tamaño de una imagen.
+    """
+    counter = 0
+    for counter, document in enumerate(input_dir.glob("*.JPG")):
+        # Crea las carpetas necesarias
+        output_document_path = make_output_document_path(document, output_dir)
 
-        contador = 0
-        for contador, archivo in enumerate(self.carpeta_origen.glob("*.JPG")):
-            # Crea las carpetas necesarias
-            direccion_destino = self._crear_destino(archivo)
+        # Lee la imagen
+        img = cv.imread(str(document))
+        # Reduce el tamaño de la imagen
+        if reduction:
+            img = change_size(img, reduction)
+            logger.debug("Reducido al %.2f%%", reduction * 100)
 
-            # Lee la imagen y la reduce
-            img = self.cambiar_tamano(cv.imread(str(archivo)), reduccion)
-
-            # Guarda la imagen en la nueva dirección
-            cv.imwrite(str(direccion_destino), img)
-            print(f"Copiado {direccion_destino.name} reducido al {reduccion:.2%}.")
-        print(f"Terminado procesado de {contador} archivos...")
+        # Guarda la imagen en la nueva dirección
+        cv.imwrite(str(output_document_path), img)
+        logger.info("Copiado %s", document.name)
+    logger.info("Terminado procesado de %s archivos...", counter)
 
 
 def main():
     """Función principal"""
     carpeta_origen = input("Nombre de la carpeta: ")
-    reduccion = float(input("Rango de reducción (0 < x < 1 ): "))
-    mueve_curp_acta = MueveCurpActa(carpeta_origen)
-    mueve_curp_acta.mover_archivos(reduccion)
+    reduction = float(input("Rango de reducción (0 < x < 1 ): "))
+    input_dir = Path(carpeta_origen)
+    output_dir = input_dir.with_name(f"{input_dir.name} Carpetas")
+
+    move_images(input_dir, output_dir, reduction)
 
 
-#%%
 if __name__ == "__main__":
     main()
