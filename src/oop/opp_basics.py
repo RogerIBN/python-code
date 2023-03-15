db13c517ff555afb62406a2be36ca4b051e8c0d9
Roger Barrera <70298206+RogerIBN@users.noreply.github.com>
2023-03-15 16:07:26 -0600

    feat: :hammer: add main method

--- src/oop/opp_basics.py
+++ src/oop/opp_basics.py
@@ -1,9 +1,13 @@
 """Programa de ejemplo de como funcionan las clases"""
 # Declaración de la clase
+from typing import Self
+
+
 class Persona:
     """Modelo de una persona"""
 
-    # __init__ es la palabra clave de la función que ejecuta una clase cuando se instancia
+    # __init__ es la palabra clave de la función que ejecuta una clase cuando
+    # se instancia
     def __init__(self, nombre: str, edad: int):
         self.nombre = nombre  # Guarda la variable nombre como atributo del objeto
         self.edad = edad  # Guarda la variable edad como atributo del objeto
@@ -13,31 +17,37 @@ class Persona:
         print(f"{self.nombre}: Mi nombre es {self.nombre} y tengo {self.edad} años")
 
     # Método del objeto mientras accede a sus atributos y los de otro objeto
-    def saludar(self, other: "Persona") -> None:
+    def saludar(self, other: Self) -> None:
         """
         Método que hace una persona salude a otra
 
         Parameters
         ----------
-        other : Persona
+        other : Self
             La persona a saludar
         """
         print(f"{self.nombre}: Hola {other.nombre}, soy {self.nombre}")
 
 
-# Crear los objetos de que pertenecen a la clase persona (instanciar)
-persona_1 = Persona(nombre="Roger", edad=24)
-persona_2 = Persona(nombre="Karla", edad=24)
+def main():
+    """Función principal del programa"""
+    # Crear los objetos de que pertenecen a la clase persona (instanciar)
+    persona_1 = Persona(nombre="Roger", edad=24)
+    persona_2 = Persona(nombre="Karla", edad=24)
 
-# Probar los métodos
-persona_1.presentarse()
-persona_2.presentarse()
+    # Probar los métodos
+    persona_1.presentarse()
+    persona_2.presentarse()
 
-persona_1.saludar(persona_2)
+    persona_1.saludar(persona_2)
 
-# Acceder a los atributos (Probamos si sus edades son iguales)
-print(
-    f"""\
-¿{persona_1.nombre} y {persona_2.nombre} tienen la misma edad?
-Respuesta: {persona_1.edad == persona_2.edad}"""
-)
+    # Acceder a los atributos (Probamos si sus edades son iguales)
+    print(
+        f"""\
+    ¿{persona_1.nombre} y {persona_2.nombre} tienen la misma edad?
+    Respuesta: {persona_1.edad == persona_2.edad}"""
+    )
+
+
+if __name__ == "__main__":
+    main()
