9295f139fc2f4900abdb9df4a2071f05f8956c9b
Roger Barrera <70298206+RogerIBN@users.noreply.github.com>
2023-12-20 03:02:14 -0600

    test: :white_check_mark: Fix subtitle conversion and add additional test case

--- src/subtitles/test/test_subs_txt_to_srt.py
+++ src/subtitles/test/test_subs_txt_to_srt.py
@@ -16,7 +16,8 @@ def test_subs_txt_to_srt() -> None:
 hola
 00:01
 adiós
-00:02\
+00:02
+vamos
 """
 
     expected_text = """\
@@ -28,9 +29,42 @@ Hola
 00:00:01,00 --> 00:00:02,00
 Adiós
 
+3
+00:00:02,00 --> 00:00:05,00
+Vamos
+
 """
     # Act
     output_text = srt_from_raw_youtube(input_text=input_text)
 
     # Assert
-    assert output_text == expected_text
+    assert output_text == expected_text, "The output text is not the expected."
+
+    input_text = """\
+00:00
+español
+00:05
+inglés
+00:10
+francés
+"""
+
+    expected_text = """\
+1
+00:00:00,00 --> 00:00:05,00
+Español
+
+2
+00:00:05,00 --> 00:00:10,00
+Inglés
+
+3
+00:00:10,00 --> 00:00:15,00
+Francés
+
+"""
+    # Act
+    output_text = srt_from_raw_youtube(input_text=input_text, seg=5)
+
+    # Assert
+    assert output_text == expected_text, "The output text is not the expected."
