dafe45f6280364c280ab9afa930625f0cede5cb0
Roger Barrera <70298206+RogerIBN@users.noreply.github.com>
2023-12-20 02:57:08 -0600

    feat: :fire: Delete unnecessary files and update solution for Python challenge

--- src/challenges/different_methods.py
+++ src/challenges/different_methods.py
@@ -1,5 +1,20 @@
 """
-This solves the challenge in 3 different ways.
+Python Question
+What method would you pick to solve this problem?
+First, pick the method, then provide solution.🤔🤔
+
+>>> heights_inches = [ 71,  75,  68,  72,  73]
+>>> weight_pounds  = [165, 190, 140, 175, 185]
+
+What is the average weight of the persons with height above 71 inches?
+How are you solving this problem?
+- (A) Solve the problem using pandas.
+- (B) Solve the problem using NumPy.
+- (c) Solve the problem using pure Python.
+
+Now, provide the answer using the method you picked.
+> Benjamin Bennett Alexander (@RealBenjizo)
+> [May 19, 2023](https://twitter.com/RealBenjizo/status/1659634156949610508?ref_src=twsrc%5Etfw)
 """
 from statistics import mean
 
@@ -11,7 +26,8 @@ weight_pounds = [165, 190, 140, 175, 185]
 
 # Using Pandas
 df = pd.DataFrame({"heights_inches": heights_inches, "weight_pounds": weight_pounds})
-pandas_ans = df.loc[lambda df_: df_["heights_inches"] > 71, "weight_pounds"].mean()
+serie: pd.Series = df.loc[lambda df_: df_["heights_inches"] > 71, "weight_pounds"]  # type: ignore
+pandas_ans = serie.mean()
 
 # Using Numpy
 metrics = np.array(
@@ -25,3 +41,4 @@ python_ans = mean(
     weight for height, weight in zip(heights_inches, weight_pounds) if height > 71
 )
 print(f"{pandas_ans=}, {numpy_ans=}, {python_ans=}")
+print(__doc__)
